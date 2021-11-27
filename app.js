6253c136222026d8fa1e267b11fb01b0610ee62c
EdgarPachinsky <www.edgar.pachinsky@gmail.com>
2021-11-27 18:57:35 +0400

    fixes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path');
 
-const constants = require('./constant/vars.js')
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'dashboard/index.html'));
 });
 
-app.listen(constants.PORT, constants.HOST, (err) => {
+app.listen(PORT, (err) => {
 
     if (!err) {
 
-        console.log(`STARTING SERVER - OK - : ${constants.PORT}`);
+        console.log(`STARTING SERVER - OK - : ${PORT}`);
 
     } else {
         console.log(`STARTING SERVER - ERROR - : ${err}`);
--- constant/vars.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SOCKET_PORT:7000,
-    HOST:'127.0.0.1',
-    PORT:3001,
-}
